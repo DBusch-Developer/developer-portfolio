a8fb3d99a8fd1f5a34bcbb4fed1c5dc05801abc2
Diana Busch <buschdiana007@gmail.com>
2025-08-25 09:34:00 -0700

    Adds Sidebar. Adds name with typing effect. Adds title. Adds avatar to assets. Adds contact info. Adds social links.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
+import Sidebar from "./components/Sidebar";
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-     
+      <div className="min-h-screen p-4">
+        <div className="max-w-7xl mx-auto flex flex-col lg:flex-row gap-6">
+          <Sidebar />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
